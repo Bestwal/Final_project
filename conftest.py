b3fb4968fa84cff6f91d6951ac23432f4fb75545
Роман <hammerfallsamara@mail.ru>
2025-06-13 20:56:53 +0400

    создали главную страницу

--- conftest.py
+++ conftest.py
@@ -2,11 +2,12 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def pytest_addoption(parser):
     parser.addoption(
         '--language',
         action='store',
-        default='ru',
+        default='en',
         help='Language code for browser locale'
     )
 
